8cf64af5b91eb148adc8022fdca1c79c54173594
HamzaJirah <hamzajirah@gmail.com>
2018-08-23 11:59:57 +0100

    added comment

--- selectors.js
+++ selectors.js
@@ -1,5 +1,5 @@
 //ID Selector (selected the HTML element with the ID "highlight")
 var tag = document.getElementById("highlight");
 
-
+//Class Selector (Selects all HTML elements with the class "bolded")
 var tags = document.getElementsByClassName("bolded");
