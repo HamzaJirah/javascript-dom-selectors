ed1817f32aeae3a2bc2446947182c8427729456c
HamzaJirah <hamzajirah@gmail.com>
2018-08-23 12:00:57 +0100

    added tag selector

--- selectors.js
+++ selectors.js
@@ -3,3 +3,6 @@ var tag = document.getElementById("highlight");
 
 //Class Selector (Selects all HTML elements with the class "bolded")
 var tags = document.getElementsByClassName("bolded");
+
+
+var liTags = document.getElementsByTagName("li");
